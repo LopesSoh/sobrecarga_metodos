f3868348aaed19d44ccaf1cceb1c74d758d9dfb9
LopesSoh <sophiac.lopes2008@gmail.com>
2025-10-07 09:00:39 -0300

    estrutura do gerar

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Gerador_Relatorios:
+    def gerar(*args, **kwargs):
+        relatorio = ""
+
+        match len(args, kwargs):
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case _:
+                pass
+            
+        if kwargs:
+            pass
